9a38249d379dbd2065530a52ce489459b80497e2
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-02-16 14:58:11 -0800

    finished dish CRUD   -also added routes

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,39 @@
+def DishesController < ApplicationController
+  before_action :dish_id, only: [:show, :edit, :update, :destroy]
+  def index
+    @dishes = Dish.all
+  end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def create
+    @dish = Dish.new
+    @dish.save
+    redirect_to dish_path(@dish)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @dish.destroy
+    redirect_to dishes_path
+  end
+
+  private
+  def dishes_params
+    params.require(:dish).permit(:name, :price, :description)
+  end
+
+  def dish_id
+    @dish = Dish.find params[:id]
+  end
+end
