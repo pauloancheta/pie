c7052eff02d85423d1bc9db9cc45e25ee2f0b6af
Zachary Meadows <zacmeadows@me.com>
2015-02-18 14:11:28 -0800

    added CRUD for menu and changed root path

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,53 @@
+class MenusController < ApplicationController
+
+  before_action :menu_id, only: [:show, :edit, :update, :destroy]
+
+  def index 
+    @menus = Menu.all
+  end 
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new menu_params
+    if @menu.save
+      redirect_to menus_path
+    else
+      render :new
+    end 
+  end 
+
+  def show 
+  end 
+
+
+  def edit
+  end 
+
+  def update
+    if @menu.update menu_params
+      redirect_to menus_path
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @menu.destroy
+    redirect_to menus_path
+  end 
+
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name)
+  end 
+
+  def menu_id
+    @menu = Menu.find params[:id]
+  end 
+
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216213520) do
+ActiveRecord::Schema.define(version: 20150218161955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,18 +34,8 @@ ActiveRecord::Schema.define(version: 20150216213520) do
 
   add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id", using: :btree
 
-  create_table "menu_items", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.integer  "menu_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "menu_items", ["menu_id"], name: "index_menu_items_on_menu_id", using: :btree
-
   create_table "menus", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -59,28 +49,6 @@ ActiveRecord::Schema.define(version: 20150216213520) do
 
   add_index "recipes", ["dish_id"], name: "index_recipes_on_dish_id", using: :btree
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "name"
-    t.string   "address"
-    t.string   "phone_number"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "restaurants", ["email"], name: "index_restaurants_on_email", unique: true, using: :btree
-  add_index "restaurants", ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true, using: :btree
-
   add_foreign_key "ingredients", "recipes"
-  add_foreign_key "menu_items", "menus"
   add_foreign_key "recipes", "dishes"
 end
