b18887526306c44ff5070467f70969d80151a5df
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-19 13:40:07 -0800

    validates email with a regular expression during registration. a newly created user's is_admin will now always evaluate to false

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
