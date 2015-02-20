ae072ca4f66089fa6e9ed34b20fa3a584817b5f6
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-19 16:52:04 -0800

    name of user will be capitalized, link to signup page on login page now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     user.name.strip!
+    user.name.capitalize!
     user.is_admin = false
     if user.save
       session[:user_id] = user.id
