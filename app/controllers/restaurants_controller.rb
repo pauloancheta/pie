10fbcafbd4f7e7409e9342536fbb4eea32a81ea4
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-22 17:45:33 -0800

    fixed spacing after add restaurant to favourites button

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,7 @@ class RestaurantsController < ApplicationController
       redirect_to user_menus_path(current_user)
     else
       @restaurants = User.all.where(is_admin: true)
+      @favourite = current_user.favourite_for(current_user)
     end
   end
 
