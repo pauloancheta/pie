c093aba57da1819a8f1b59621f9362088fc9e89f
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-02-25 12:30:55 -0800

    refactored recipes accordion   -fixed bug in the root path

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -4,18 +4,4 @@ $(document).ready(function() {
     $('.recipeForm').fadeToggle();
   });
 
-  $('#recipeLink').hide();
-
-  $('#recipeRow').on('click', function() {
-    $('#recipeLink').fadeOut();
-  });
-
-  $('#recipeRow').on('mouseenter', function() {
-    $('#recipeLink').fadeIn();
-  });
-
-  $('#recipeRow').on('mouseleave', function() {
-    $('#recipeLink').fadeOut();
-  });
-
 });
