0e08fa315107f2fa1ddd714ad592d8427ac98296
Zachary Meadows <zacmeadows@me.com>
2015-02-24 23:01:47 -0800

    make recipe edit/delete links hide/show on recipe hover using jquery

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -4,4 +4,18 @@ $(document).ready(function() {
     $('.recipeForm').fadeToggle();
   });
 
+  $('#recipeLink').hide();
+
+  $('#recipeRow').on('click', function() {
+    $('#recipeLink').fadeOut();
+  });
+
+  $('#recipeRow').on('mouseenter', function() {
+    $('#recipeLink').fadeIn();
+  });
+
+  $('#recipeRow').on('mouseleave', function() {
+    $('#recipeLink').fadeOut();
+  });
+
 });
