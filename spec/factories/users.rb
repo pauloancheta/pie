b2ca500fa4f5e8eb77df2138ae2afdf72bb20f63
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-28 14:56:52 -0800

    created validation tests and a password digest test for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :user do |f|
+    f.name { Faker::Name.name }
+    f.address { Faker::Address.street_name }
+    f.phone_number { Faker::PhoneNumber.phone_number }
+    f.email { Faker::Internet.email }
+    f.password 'lol123'
+    f.password_confirmation 'lol123'
   end
 
 end
