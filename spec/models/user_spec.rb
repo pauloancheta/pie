cd4654d881d3f71b38c019b922802d16d9dea659
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-28 13:51:47 -0800

    setup factory girl and rspec, beginning tests for the user model now

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "has a valid factory"
+  it "is invalid without a name"
+  it "is invalid without an email"
+  it "is invalid without a password"
+end
