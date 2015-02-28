b6a9d7f412f7412557c3c6b6f046be6386999c67
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-28 15:11:08 -0800

    added a test for password digest presence as admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,19 @@ RSpec.describe User, type: :model do
     it "has a valid factory" do
       expect(FactoryGirl.create(:user)).to be_valid
     end
+
     it "is invalid without a name" do
       expect(FactoryGirl.build(:user, name: nil)).to be_invalid
     end
+
     it "is invalid without an email" do
       expect(FactoryGirl.build(:user, email: nil)).to be_invalid
     end
+
     it "is invalid without a password" do
       expect(FactoryGirl.build(:user, password: nil)).to be_invalid
     end
+
     it "is invalid without a password confirmation" do
       expect(FactoryGirl.build(:user, password_confirmation: nil)).to be_invalid
     end
@@ -24,8 +28,22 @@ RSpec.describe User, type: :model do
     def user_params
       params.require(:user).permit(:name, :address, :phone_number, :email, :password, :password_confirmation, :is_admin)
     end
-    it "generate password digest when given password" do
+    
+    it "generate password digest when given password as regular user" do
       user = User.create(name:'bran',
+                         address:'123 lol road',
+                         email: 'brandyn.prasad@gmail.com',
+                         phone_number: '123 123 1234',
+                         password: 'lol123',
+                         password_confirmation: 'lol123'
+                         )
+      user.save
+      expect(user.password_digest).to be
+    end
+
+    it "generate password digest when given password as an admin user" do
+      user = User.create(is_admin: true,
+                         name:'bran',
                          address:'123 lol road',
                          email: 'brandyn.prasad@gmail.com',
                          phone_number: '123 123 1234',
