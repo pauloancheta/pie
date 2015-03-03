9c550b21ed52bbc91933104a2dd2bad4f97932b1
Zachary Meadows <zacmeadows@me.com>
2015-03-03 00:20:25 -0800

    inclusion/ingredient model tests

---
+++ spec/factories/inclusions.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :inclusion do
+    recipe_id 3
+    ingredient_id 3
+  end
+end
--- spec/models/inclusion_spec.rb
+++ spec/models/inclusion_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe DishRecipe, type: :model do
+RSpec.describe Inclusion, type: :model do
 
   context 'association' do
     it { should belong_to(:recipe) }
-    it { should belong_to(:dish) }
+    it { should belong_to(:ingredient) }
   end
 
   context 'column specification' do
     it { should have_db_column(:recipe_id).of_type(:integer) }
-    it { should have_db_column(:dish_id).of_type(:integer) }
+    it { should have_db_column(:ingredient_id).of_type(:integer) }
   end
 
 end
