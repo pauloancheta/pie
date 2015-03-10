9f9742c5072381d92cfff743d32eed9f65e5ad62
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-03-10 00:18:07 -0700

    added categories to recipes with tabbed view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301224451) do
+ActiveRecord::Schema.define(version: 20150310043702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20150301224451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "category"
   end
 
   add_index "recipes", ["user_id"], name: "index_recipes_on_user_id", using: :btree
