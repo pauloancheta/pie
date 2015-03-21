be21d2afa1c4d8f39175828ab442f2463083ee9e
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-03-21 13:10:52 -0700

    Added warnings for delete

--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -3,6 +3,10 @@ class InclusionsController < ApplicationController
     ingredient = Ingredient.find params[:ingredient_id]
     recipe = Recipe.find params[:recipe_id]
     Inclusion.where(recipe_id: recipe.id, ingredient_id: ingredient.id).first.destroy
-    redirect_to recipes_path
+    if current_user.is_admin
+      redirect_to recipes_path
+    else
+      redirect_to preference_path(current_user.preference.id)
+    end
   end
 end
